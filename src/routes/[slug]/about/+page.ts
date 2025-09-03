c040d8c04f3c54655089891d143248750a5d837b
Mqlvin <pricklydotcactus@gmail.com>
2025-09-03 17:53:40 +0100

    test whoami

--- src/routes/[slug]/about/+page.ts
+++ src/routes/[slug]/about/+page.ts
@@ -1,5 +1,9 @@
 export async function load({ params, fetch }): Promise<{ ip: string, slug: string }> {
-    const res = await fetch("https://api.ipify.org?format=json");
+    const res = await fetch("https://ctrlclub.mqlvin.dev/auth/whoami", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+    });
     const post = await res.json();
     return { ip: post.ip, slug: params.slug };
 }
