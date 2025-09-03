97025c635fe3a8020165411763454f3b0fe747fa
Mqlvin <pricklydotcactus@gmail.com>
2025-09-03 18:49:05 +0100

    add logout button to homepage

--- src/routes/challenges/+page.svelte
+++ src/routes/challenges/+page.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+    async function logout() {
+        await fetch("https://ctrlclub.mqlvin.dev/auth/logout", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include"
+        });
+
+        window.location.href = "/login";
+    }
+</script>
+
+<button on:click={() => { logout(); }}> Logout </button>
 <h1>challenges:</h1>
