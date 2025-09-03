ffdf6004e2c497c6a1f428e2f9541e8e7733249a
Mqlvin <pricklydotcactus@gmail.com>
2025-09-03 18:10:17 +0100

    remove old loggin

--- src/routes/[slug]/about/+page.ts
+++ src/routes/[slug]/about/+page.ts
@@ -1,9 +1,3 @@
 export async function load({ params, fetch }): Promise<{ ip: string, slug: string }> {
-    const res = await fetch("https://ctrlclub.mqlvin.dev/auth/whoami", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include"
-    });
-    const post = await res.json();
-    return { ip: post.ip, slug: params.slug };
+    return { ip: "hi", slug: params.slug };
 }
